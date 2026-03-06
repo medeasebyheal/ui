72e9896fdcf61b20fd4c322b63b27f6e75faf859
owais <bcitgeek@gmail.com>
2026-03-06 08:03:42 +0500

    asd

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
-console.log(API_BASE);
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 export const api = axios.create({
   baseURL: API_BASE,
   headers: { 'Content-Type': 'application/json' },
